909557c75f517b3bde64e885381eab64939a7f28
Theflow-one <cstubbs270@gmail.com>
2025-08-18 13:12:43 -0400

    Create task.js

---
+++ task.js
@@ -0,0 +1,13 @@
+// models/Task.js
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
