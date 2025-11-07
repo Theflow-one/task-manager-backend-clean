4d4af4be79dc4bba451fe32c68a8c13f2ad78967
Theflow-one <cstubbs270@gmail.com>
2025-11-07 08:34:18 -0500

    Update task.js

--- task.js
+++ task.js
@@ -1,12 +1,1 @@
-import mongoose from 'mongoose';
 
-const taskSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-});
